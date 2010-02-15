8c1b8baed1e22b0ac449c1881821187c441edaa9
Lloyd Pick <lloydpick@gmail.com>
2010-02-15 20:27:15 +0000

    correct the git branches

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,          "production"
+set :branch,          "master"
 set :rails_env,       'production'
 set :domain,          "bugs.limi.co.uk"
 set :domain_aliases,  "bugs.production.limi.co.uk"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-set :branch,          "staging"
+set :branch,          "master"
 set :rails_env,       'staging'
 set :domain,          "bugs.staging.limi.co.uk"
 set :domain_aliases,  "www.bugs.staging.limi.co.uk"
