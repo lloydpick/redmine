4058f4259c53be0b97626ec06214a90d37eb4d3f
Lloyd Pick <lloydpick@gmail.com>
2010-02-19 19:04:38 +0000

    updated git branch for deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :branch,          "master"
+set :branch,          "master_limi"
 set :rails_env,       'production'
 set :domain,          "bugs.limi.co.uk"
 set :domain_aliases,  "bugs.production.limi.co.uk"
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
-set :branch,          "master"
+set :branch,          "master_limi"
 set :rails_env,       'staging'
 set :domain,          "bugs.staging.limi.co.uk"
 set :domain_aliases,  "www.bugs.staging.limi.co.uk"
